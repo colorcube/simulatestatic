a30397b6bcf5b00f95c2204a9357eb9ec01aa38b
René Fritz <r.fritz@colorcube.de>
2016-12-12 08:48:26 +0100

    This is version 2.0.0 from ter

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "simulatestatic".
+#
+# Auto generated 14-08-2015 17:39
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Simulate Static URLs',
+	'description' => 'Adds the possibility to have Speaking URLs in the TYPO3 Frontend pages.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '6.0.0-0.0.0',
+	'PHP_version' => '5.3.0-0.0.0',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Benjamin Mack',
+	'author_email' => 'benni@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
